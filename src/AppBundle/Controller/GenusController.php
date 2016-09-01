b23c0bb4cf1b5e58ac8df29adddc7da7eb3f249c
xavier.lis <xavier.lis@gmail.com>
2016-09-01 23:27:03 +0300

    6. Listing and Using Services

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -3,7 +3,6 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
 {
@@ -12,10 +11,8 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-        $templating = $this->container->get('templating');
-        $html = $templating->render('genus/show.html.twig', array(
+        return $this->render('genus/show.html.twig', array(
             'name' => $genusName
         ));
-        return new Response($html);
     }
 }
