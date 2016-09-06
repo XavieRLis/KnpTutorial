0337d61a831aea9cc8c4564a6c70649ac648cf9a
xavier.lis <xavier.lis@gmail.com>
2016-09-06 22:29:33 +0300

    2. The King of Relations: ManyToOne Course: Mastering Doctrine Relationships in Symfony Tutorial

--- src/AppBundle/Entity/GenusNote.php
+++ src/AppBundle/Entity/GenusNote.php
@@ -34,6 +34,11 @@ class GenusNote
     private $createdAt;
 
     /**
+     * @ORM\ManyToOne(targetEntity="Genus")
+     */
+    private $genus;
+
+    /**
      * @return mixed
      */
     public function getUsername()
