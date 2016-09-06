cca83a42a15a45933fed760d41dbe652e1d815ac
xavier.lis <xavier.lis@gmail.com>
2016-09-06 21:51:53 +0300

    6. Query for a List of Genuses Course: Symfony: Doctrine & the Database Tutorial

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -28,6 +28,18 @@ class GenusController extends Controller
     }
 
     /**
+     * @Route("/genus")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $genuses = $em->getRepository('AppBundle\Entity\Genus')
+            ->findAll();
+             dump($genuses);die;
+    }
+
+    /**
      * @Route("/genus/{genusName}")
      */
     public function showAction($genusName)
