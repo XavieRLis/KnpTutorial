e002bc80d11007634f7221dd75739efb08849271
xavier.lis <xavier.lis@gmail.com>
2016-09-20 22:42:03 +0300

    4. Register your Service in the Container  Course: Symfony: Level up with Services and the Container Tutorial

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -65,9 +65,7 @@ class GenusController extends Controller
         }
         $recentNotes = $em->getRepository('AppBundle:GenusNote')
             ->findAllRecentNotesForGenus($genus);
-        $markdownTransformer = new MarkdownTransformer(
-            $this->get('markdown.parser')
-        );
+        $markdownTransformer = $this->get('app.markdown_transformer');
         $funFact = $markdownTransformer->parse($genus->getFunFact());
 //        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
 //        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
